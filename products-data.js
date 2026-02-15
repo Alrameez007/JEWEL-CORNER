c346abf1c1f77bf850248a2d5da0a2377afe5238
Alrameez007 <sideekalrameez@gmail.com>
2026-02-15 20:31:18 +0400

    Create products-data.js

---
+++ products-data.js
@@ -0,0 +1,31 @@
+const products = [
+
+    {
+        name: "Luxury Gold Ring",
+        category: "rings",
+        price: "12 OMR",
+        image: "images/ring1.jpg"
+    },
+
+    {
+        name: "Diamond Style Ring",
+        category: "rings",
+        price: "18 OMR",
+        image: "images/ring2.jpg"
+    },
+
+    {
+        name: "Premium Ladies Watch",
+        category: "watches",
+        price: "25 OMR",
+        image: "images/watch1.jpg"
+    },
+
+    {
+        name: "Royal Oud Perfume",
+        category: "perfumes",
+        price: "20 OMR",
+        image: "images/perfume1.jpg"
+    }
+
+];
