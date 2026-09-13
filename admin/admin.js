c54a8032204c2570521321a76dff0b347f47cc50
Alrameez007 <sideekalrameez@gmail.com>
2026-09-14 00:41:42 +0400

    Change inactivity limit to 10 minutes

--- admin/admin.js
+++ admin/admin.js
@@ -123,7 +123,7 @@ logoutButton.addEventListener(
    AUTO LOGOUT AFTER 10 MINUTES OF INACTIVITY
    ========================================================= */
 
-const INACTIVITY_LIMIT = 30 * 1000;
+const INACTIVITY_LIMIT = 10 * 60 * 1000;
 
 let inactivityTimer;
 function resetInactivityTimer() {
